e767dc650cb9905ea11b3847f340072133fafe33
Achintya Maiti <achintyamaiti8972@gmail.com>
2025-08-24 20:05:47 +0530

    goback arrow

--- src/screens/PanVerification/index.tsx
+++ src/screens/PanVerification/index.tsx
@@ -12,13 +12,24 @@ import {
 import React from 'react';
 import { Icons } from '../../themes/Icons';
 import { styles } from './styles';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RootMainStackParamList } from '../../types';
+import { useNavigation } from '@react-navigation/native';
+
+type PanVerificationNavProp = NativeStackNavigationProp<
+  RootMainStackParamList,
+  'PanVerification'
+>;
+
 
 const PanVerification = () => {
+      const navigation = useNavigation<PanVerificationNavProp>();
+
   return (
     <SafeAreaView style={styles.main}>
       {/* Header Container */}
       <View style={styles.header}>
-        <TouchableOpacity style={styles.touchArrow}>
+        <TouchableOpacity onPress={navigation.goBack} style={styles.touchArrow}>
           <Icons.arrowIcon style={styles.arrowIcon} />
         </TouchableOpacity>
         <Text style={styles.title}>Pan Verification</Text>
