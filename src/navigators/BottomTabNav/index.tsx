1bcffb30e3dba20fcf24ab38420872295de71737
Achintya Maiti <achintyamaiti8972@gmail.com>
2025-09-25 09:01:27 +0530

    add

--- src/navigators/BottomTabNav/index.tsx
+++ src/navigators/BottomTabNav/index.tsx
@@ -90,6 +90,8 @@
 // }
 
 
+//it
+
 import React, { useState } from 'react';
 import { Dimensions, View, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { TabView, SceneMap } from 'react-native-tab-view';
